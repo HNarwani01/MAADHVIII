43b3a44dfd5c74a409bfe88fe7d8138bd344f94e
HNarwani01 <himanshurawat0075407@gmail.com>
2024-03-27 20:26:57 +0530

    nav-links

--- script/common.js
+++ script/common.js
@@ -1,3 +1,12 @@
 document.querySelector('.starting-point .name-heading .name').addEventListener('click', function () {
     window.location.href = 'index.html';
 });
+document.querySelector('.starting-point .name-heading img').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+document.querySelector('.phone-point .phone-point-line-1 h1').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+document.querySelector('.phone-point .phone-point-line-1 img').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
